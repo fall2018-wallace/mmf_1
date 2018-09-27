efd4c87b5f4bc63ba200bf5e7ebb4a459126864f
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-09-27 13:05:05 -0400

    Saving download_data.R

--- download_data.R
+++ download_data.R
@@ -1,1 +1,10 @@
 
+CleandfStates <- function()
+{
+    dfStates <- read.csv(url("https://www2.census.gov/programs-surveys/popest/datasets/2010-2017/state/asrh/scprc-est2017-18+pop-res.csv"))
+    dfStates <- dfStates[,c(5,6,7,8)]
+    dfStates <- dfStates[2:52,]
+    dfStates <-setNames(dfStates,c("stateName", "population", "popOver18", "percentOver18"))
+
+    return (dfStates)
+}
